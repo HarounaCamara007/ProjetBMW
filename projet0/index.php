dcca76ddff3694c62b1230b718a9e75bab917242
HarounaCamara007 <108951970+HarounaCamara007@users.noreply.github.com>
2022-10-24 11:41:47 +0200

    Add files via upload

---
+++ projet0/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Css/projet.css"/> 
+    <title>Projet</title>
+</head>
+<body>
+
+    <div class ="header">
+    <a href= "#bmwM2"><img id = "logoM2" src ="./img/m2logo.PNG"></img></a>
+    </div>
+    
+
+    <div id="bmwDiv">
+        
+        
+        <img id="bmwM2" src= "https://www.largus.fr/images/images/2018-bmw-m2-competition-01.jpg"></img>
+        <img id = "M2Back" src ="./img/bmw-m2-back.jpg"></img>
+    <!-- <button id= "ButtonPlay"> <a href="#vehicule">PLAY </a></button> -->
+
+<div class ="perf">
+    <div class = "info">
+    <div id ="info1"> <p>Model</p> </div>
+    <div id ="info2"> <p>Puissance</p> </div>
+    <div id ="info3"> <p>0 à 100</p> </div>
+    <div id ="info4"> <p>Prix de base</p> </div>
+    <div id ="info5"> <p>Début de commercialisation</p> </div>
+    <div id ="info6"> <p>Moteur</p> </div>
+    </div>
+
+    <div class = "infoReponse">
+    <div id ="info1Reponse"> <p>BMW M2 (F87 Coupé)</p> </div>
+    <div id ="info2Reponse"> <p>370 ch</p> </div>
+    <div id ="info3Reponse"> <p>4.3 s</p> </div>
+    <div id ="info4Reponse"> <p>65.960 €</p> </div>
+    <div id ="info5Reponse"> <p>2016</p> </div>
+    <div id ="info6Reponse"> <p>6 cylindres en ligne, </br>24 soupapes</p> </div>
+    </div>
+</div>
+
+</div>
+
+<img id = "bandeM" src ="./img/bandeM.jpg"></img>
+
+
+
+    <div id ="vehicule">
+     <p>ek,ddk</p>
+    </div>
+    
+</body>
+</html>
